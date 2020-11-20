a4b02ed5e3ed3c95ef10e82a53dbc3c69cc3a117
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-19 19:05:05 -0800

    Add files via upload

--- adventurers/urls.py
+++ adventurers/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:character_id>/player/', views.player, name='player'),
+    path('create/', views.create, name='create'),
 ]
--- adventurers/views.py
+++ adventurers/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("Main Adventure Page")
+    return render(request, 'adventurers/index.html', {})
+
+def player(request, character_id):
+    return HttpResponse("Character you are playing")
+
+def create(request):
+    return HttpResponse("Add new character")
