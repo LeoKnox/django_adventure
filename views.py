03b5960d9b9769d7fbb649774d33fb28d1dd5c67
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-22 21:02:14 -0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+from .models import Adventurer
+
+def index(request):
+    adv_data = Adventurer.objects.values()
+    return render(request, 'adventurers/index.html', {'adv_data': adv_data})
+
+def player(request, character_id):
+    adv = Adventurer.objects.filter(id=character_id).first()
+    return render(request, 'adventurers/player.html', {'adv':adv})
+
+def create(request):
+    return render(request, 'adventurers/create.html')
+
+def creater(request):
+    print ('yo yo yo!')
+    return render(request, 'adventurers/index.html')
